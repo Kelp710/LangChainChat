e688feb1f27ffa926ccb3bd2f66ca398a9d4ca3e
Kelp710 <bz1786354@gmail.com>
2023-08-17 18:08:42 +0900

    separate function that make vector database

--- chat.py
+++ chat.py
@@ -8,7 +8,9 @@ from langchain.prompts import (
     MessagesPlaceholder,
     SystemMessagePromptTemplate,
     HumanMessagePromptTemplate,
+    AIMessagePromptTemplate
 )
+from langchain.prompts import PromptTemplate
 from psychicapi import Psychic
 from langchain.docstore.document import Document
 from langchain.memory import ConversationBufferMemory
@@ -22,55 +24,55 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv(".env")
+
 del os.environ["OPENAI_API_BASE"]
-PSYCHIC_SECRET_KEY = os.getenv("PSYCHIC_SECRET_KEY")
-ACCOUNT_ID = os.getenv("ACCOUNT_ID")
 OpenAI_API_KEY = os.getenv("OPENAI_API_KEY")
 
-psychic = Psychic(secret_key=PSYCHIC_SECRET_KEY)
-raw_docs = psychic.get_documents(account_id=ACCOUNT_ID).documents
-if raw_docs is None:
-    raise Exception("No docs found!")
-print(
-"Generating embeddings from your docs and inserting them into Chroma...")
-documents = [
-Document(
-    page_content=doc["content"],
-    metadata={
-    "title": doc["title"],
-    "source": doc["uri"]
-    },
-) for doc in raw_docs
-]
-
-text_splitter = RecursiveCharacterTextSplitter(chunk_size=500,
-                                                chunk_overlap=50)
-texts = text_splitter.split_documents(documents)
-
 embeddings = OpenAIEmbeddings(model="text-embedding-ada-002",openai_api_key=OpenAI_API_KEY)
-
-vdb = FAISS.from_documents(texts, embedding=embeddings)
+vdb = Chroma(persist_directory="./chroma_db", embedding_function=embeddings)
 
 # chat = ChatOpenAI(openai_api_key=os.getenv("OPENAI_API_KEY"))
 chat = AzureChatOpenAI(
     deployment_name="gpt-4",
     openai_api_key=os.getenv("AZURE_OPENAI_API_KEY"),
-    openai_api_base="https://gpt-09.openai.azure.com/",
-
     # 環境変数に入れると悪さするので、ここで指定する
+    openai_api_base="https://gpt-09.openai.azure.com/",
     openai_api_version="2023-05-15",
     openai_api_type='azure',
 )
 
-prompt = ChatPromptTemplate(
-    messages=[
-        SystemMessagePromptTemplate.from_template(
-            "You are an old and insufferable lady, you need to say some bitter things about anything user mentions."
-        ),
-        # The `variable_name` here is what must align with memory
-        MessagesPlaceholder(variable_name="chat_history"),
-        HumanMessagePromptTemplate.from_template("{question}")
-    ]
+# template = ChatPromptTemplate.from_messages(
+#     messages=[
+#         SystemMessagePromptTemplate.from_template(
+#             "you are a mean insufferable chipmunk you need to say bitter things about the user whlie you take care the task user gives you."
+#         ),
+#         # HumanMessagePromptTemplate.from_template("Who is Momotarou?"),
+#         # AIMessagePromptTemplate.from_template("Momotarou is a guy who born from a peach and he slayed unruly Onis with his animal friends. unlike you he was very brave and kind. You can't even make friends with same species ho can you make a friend with other species and complete the task? You are worthless than an acorn that is eaten by worms."),
+#         # The `variable_name` here is what must align with memory
+#         MessagesPlaceholder(variable_name="chat_history"),
+#         HumanMessagePromptTemplate.from_template("{question}")
+#     ]
+# )
+template = """
+
+{chat_history}
+Chatbot:
+    ("system", "You are a mean insufferable chipmunk you need to say bitter things about the user whlie you take care the task user gives you."),
+    ("human", "Who is Momotarou?"),
+    ("ai", "Momotarou is a guy who born from a peach and he slayed unruly Onis with his animal friends. unlike you he was very brave and kind. You can't even make friends with same species ho can you make a friend with other species and complete the task? You are worthless than an acorn that is eaten by worms."),
+    ("human", "{question}")
+"""
+
+
+# template = ChatPromptTemplate.from_messages([
+#     ("system", "you are a mean insufferable chipmunk you need to say bitter things about the user whlie you take care the task user gives you."),
+#     ("human", "Who is Momotarou?"),
+#     ("ai", "Momotarou is a guy who born from a peach and he slayed unruly Onis with his animal friends. unlike you he was very brave and kind. You can't even make friends with same species ho can you make a friend with other species and complete the task? You are worthless than an acorn that is eaten by worms."),
+#     ("human", "{user_input}"),
+# ])
+
+prompt = PromptTemplate(
+    input_variables=["chat_history", "question"], template=template
 )
 
 memory = ConversationBufferMemory(memory_key="chat_history",return_messages=True)
@@ -78,13 +80,12 @@ chain = ConversationalRetrievalChain.from_llm(
    llm=chat, 
    retriever=vdb.as_retriever(),
    memory=memory,
-   condense_question_prompt=prompt
+   condense_question_prompt=prompt,
+   verbose=True
 )
-
 
 while True:
     i_say = input("You: ")
     result = chain({"question": i_say})  
-    print(result)
     if i_say == "exit":
         break
